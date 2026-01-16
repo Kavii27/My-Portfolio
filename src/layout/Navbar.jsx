8af0da507909750ba1dab8d460bdd54f5327ff32
Kaveesha Sandeepani <kaveeshasandeepani027@gmail.com>
2026-01-16 22:08:34 +0530

    update navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,41 @@
+const navLinks = [
+    {href: "#about", label: "About"},
+    {href: "#projects", label: "Projects"},
+    {href: "#contact", label: "Contact"}
+]
+
 export const Navbar = () => {
+    const handleCvDownload = () => {
+
+    }
+
     return(
        <header>
-        
+            <nav>
+                <a href="#">Kaveesha</a>
+                {/* Desktop Nav */}
+                <div>
+                    <div>
+                        {navLinks.map((link, index) => (
+                            <a href={link.href} key={index}>{link.label}</a>
+                        ))}    
+                    </div>
+                </div>
+
+                <div>
+                    <button onClick={handleCvDownload}>Download CV</button>
+                    <button>DarkMode</button>
+                </div>
+
+                {/* Mobile Menu Button */}
+                <div className="md:hidden">
+                    <div>
+                        {navLinks.map((link, index) => (
+                            <a href={link.href} key={index}>{link.label}</a>
+                        ))}    
+                    </div>
+                </div>
+            </nav>
        </header>
     )
 }
